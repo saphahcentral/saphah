2c56662ae6e39f954bc475945858e59549ac9919
Saphah Central Services (HUB) <bessingerbackup2024@gmail.com>
2025-10-08 09:08:01 +0200

    Update includes-public.js with correct links

--- includes-public.js
+++ includes-public.js
@@ -1,21 +1,21 @@
 // includes-public.js
 document.addEventListener("DOMContentLoaded", () => {
   // Load header
-  fetch("https://saphahcentral.github.io/saphah/includes/header.html")
+  fetch("header.html")
     .then(res => res.text())
     .then(html => {
       document.querySelector("header")?.insertAdjacentHTML("afterbegin", html);
     });
 
   // Load navigation
-  fetch("https://saphahcentral.github.io/saphah/includes/nav.html")
+  fetch("nav.html")
     .then(res => res.text())
     .then(html => {
       document.querySelector("nav")?.insertAdjacentHTML("afterbegin", html);
     });
 
   // Load footer
-  fetch("https://saphahcentral.github.io/saphah/includes/footer.html")
+  fetch("footer.html")
     .then(res => res.text())
     .then(html => {
       document.querySelector("footer")?.insertAdjacentHTML("afterbegin", html);
